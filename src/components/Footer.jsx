5e33b62b8e57b87adbd169635e6d1f5efdbcd778
edualt <eduardosebastianhdz503@gmail.com>
2022-09-27 22:23:58 -0500

    feat(footer info): add new info

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,19 @@ import '../assets/styles/Footer.css';
 const Footer=()=> {
     return (
         <footer className="footer">
-            <a >Programacion web</a>
+            <div className="footer-container">
+                <div className="footer-left">
+                    <a >Síguenos</a>
+                </div>
+                <div className="footer-center">
+                    <h2 >KIKE.com</h2>
+                    <p>Teléfono: 01 800 123 4567</p>
+                </div>
+                <div className="footer-right">
+                    <a>hola</a>
+                </div>
+            </div>
+
         </footer>
     );
 }
