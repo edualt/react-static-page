17adc0e5839e627d4398a8941571a7dace85dedb
edualt <eduardosebastianhdz503@gmail.com>
2022-09-27 22:24:48 -0500

    feat(better code)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,11 @@
+import logo from '../assets/imgs/logo.png';
 import '../assets/styles/Header.css';
-import logo from '../assets/imgs/logo.png'
 
 const Header=()=> {
     return (
         <header>
             <nav className='navigation'>
                 <h1>KIKE<img className='imagen' src={logo}/></h1>
-                {/* <ul>
-                    <li><a href="/">Completed</a></li>
-                    <li><a href="/">Incompleted</a></li>
-                    <li><a href="/add">Add Task</a></li>
-                </ul> */}
             </nav>
         </header>
       );
